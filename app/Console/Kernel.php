466f99706051595c85adcd11de99660d610f25b1
elzifire <zenscilla@gmail.com>
2025-08-05 15:45:48 +0700

    deleting config in console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Http\Controllers\FirebaseController;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,13 +13,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(function () {
-            app(FirebaseController::class)->checkNewNews();
-        })->everyMinute();
-
-        $schedule->call(function () {
-            app(FirebaseController::class)->scheduleEventNotifications();
-        })->everyMinute();
     }
 
     /**
